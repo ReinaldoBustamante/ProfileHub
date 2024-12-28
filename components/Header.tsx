4300795223dfa3969d82ff3f48025e245dad5e59
Reinaldo Bustamante <reinaldo.bustamante026@gmail.com>
2024-12-28 12:41:01 -0300

    fix: fix attempts numbers

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export const Header = ({remaining, limit}: {remaining: number, limit: number}) => {
+    console.log(remaining)
     return (
         <header className="flex justify-between items-center">
             <span className="text-white font-bold text-[20px]">ProfileHub</span>
-            <p className="text-[20px] px-3 py-2 rounded bg-[#76BBFF] text-white">{remaining / 2}/{limit / 2}</p>
+            <p className="text-[20px] px-3 py-2 rounded bg-[#76BBFF] text-white">{Math.floor(remaining / 2)}/{Math.floor(limit / 2)}</p>
         </header>
     )
 }
